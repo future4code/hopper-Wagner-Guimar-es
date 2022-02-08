6ba8e0d193bb979b704c7fdb865fa700d82a96f2
Wagner <wagnerluizsg@outlook.com.br>
2022-02-08 19:17:04 -0300

    arquivo do vs code aula variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,82 @@
+//Exercícios-de-interpretação-de-código
+
+/*
+Atividade-número-01
+
+    console.log(b) = 10
+    console.log(a,b) = 10 10 
+
+Atividade-número-02
+    
+    console.log(a, b, c) = 10 10 10
+
+Atividade-número-03
+
+    let cargaHoraria = prompt ("Quantas horas você trabalha por dia?")
+    let diária = prompt ("Quanto você recebe por dia?")
+
+    alert(`Voce recebe ${cargaHoraria/diária} por hora`)
+
+*/
+
+//Exercícios-de-escrita-de-código
+
+/*  const   nome = null
+    const   age  = null
+
+    console.log("variante nome =", typeof nome)
+    console.log("variante age = ", typeof age)
+
+*/
+
+//quando uma variante não tem características predeterminadas elas são declaradas objetos
+
+/*
+const nome = prompt("Qual o seu nome?")
+const age = prompt("Qual a sua idade?")
+
+    console.log("variante nome =", typeof nome)
+    console.log("variante age = ", typeof age)
+
+//as variantes agora são strings
+
+alert(`Olá ${nome} você tem ${age} anos.`)
+*/
+
+/*Exercício-02
+
+    const P1 = prompt("Você gosta da cor vermelha?")
+    const P2 = prompt("Você ja foi para fora do País?")
+    const P3 = prompt("Você mora sozinho (a)?" )
+
+    alert(`Você gosta da cor vermelha? - ${P1} `)
+    alert(`Você ja foi para fora do País? - ${P2}`)
+    alert(`Você mora sozinho (a)? - ${P3}`)
+
+*/
+
+/*
+Exercícios-03
+
+    let a = 10
+    let b = 25
+
+c = a 
+a = b
+b = c  
+
+    console.log("O novo valor de a é", a)
+    console.log("O novo valor de b é", b)
+
+*/
+
+//Desafio
+
+const numero1a10 = Number(prompt("Escreva um número de 1 a 10"))
+const numero11a30 = Number(prompt("Escreva um número de 11 a 30"))
+
+alert(`Somamos o seu número ${numero1a10} com o outro número informado ${numero11a30} e o resultado foi ${numero1a10+numero11a30}`) 
+alert(`Multiplicamos o seu número ${numero1a10} com o outro número informado ${numero11a30} e o resultado foi ${numero1a10*numero11a30}`) 
+
+
+
