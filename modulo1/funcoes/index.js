7607e48b9a89159b75686256487f0877f545aab2
Wagner <wagnerluizsg@outlook.com.br>
2022-02-17 21:49:38 -0300

    funções

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,102 @@
+//Aula-Funções
+
+    //Exercícios-de-interpretação
+
+
+/*Exercício-01
+
+    Letra-A
+
+    console.log(minhaFuncao(2))         // resultado = 10
+    console.log(minhaFuncao(10))        // resultado = 50
+
+
+    Letra-B
+
+    Não iria ser impresso nada no console.
+
+*/
+
+
+/*Exercício-02
+
+    Letra-A 
+
+    pede para o usuário escrever um texto;
+    se tiver a palavra cenoura na frase informada ela deixa todos os caracteres em minúsculo.
+
+    Letra-B
+
+    true
+    true
+    true
+*/  
+
+/*Exercício-de-escrita-de-código
+
+Número-01 
+
+let dados = (nome,idade,cidade,job) => {
+    console.log(`Oi, eu sou o ${nome},tenho ${idade} anos, moro em ${cidade}, e sou ${job}`)
+
+}
+            const nome = prompt("Qual o seu nome?")
+            const idade = Number(prompt("Qual a sua idade"))
+            const cidade = prompt("Qual cidade você mora?")
+            const job = prompt("Qual a sua profissão?")
+
+dados(nome,idade,cidade,job)
+
+*/
+
+/*Número-02
+
+let exercicio = (num1,num2) => {
+    console.log(num1+num2)
+    console.log(num1>num2)
+
+}
+
+exercicio(5,10)
+
+
+
+let mensagem = (frase01) => {
+   const mensagemString = (frase01.toString()) 
+      console.log(mensagemString)
+const mensagemCount = (frase01.length)
+      
+   
+}
+
+mensagem("a minha frase é essa".toString())
+
+const num1 = Number(prompt("Fale um número de 1 a 10"))
+const num2 = Number(prompt("Fale um número de 11 a 20"))
+
+let soma = (num1, num2) => {
+    console.log(`soma: ${num1+num2}`)
+}
+
+soma(num1, num2)
+
+let subtração = (num1, num2) => {
+    console.log(`subtração: ${num1-num2}`)
+}
+
+subtração(num1, num2)
+
+let multiplicação = (num1, num2) => {
+    console.log(`multiplicação: ${num1*num2}`)
+}
+
+multiplicação(num1, num2)
+
+
+let divisão = (num1, num2) => {
+    console.log(`multiplicação: ${num1/num2}`)
+}
+
+divisão(num1, num2) 
+*/
+
