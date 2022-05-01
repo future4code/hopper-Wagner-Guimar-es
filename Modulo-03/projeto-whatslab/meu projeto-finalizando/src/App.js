5d150a272bd47036539bd5591b1fa8389cf75bf1
Wagner Luiz SG <wagnerluizsg@outlook.com.br>
2022-05-01 16:11:02 -0300

    whats-lab-finalizado

---
+++ Modulo-03/projeto-whatslab/meu projeto-finalizando/src/App.js
@@ -0,0 +1,17 @@
+import MeuChat from "../src/components/MeuChat" 
+import styled from "styled-components"
+import WhatsLab from "../src/components/MeuChat";
+
+import './App.css';
+
+function App() {
+
+return (
+  <WhatsLab />
+)
+}
+  
+  
+
+
+export default App;
